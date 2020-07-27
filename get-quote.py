9455f52cdb2fa1fe777d21c392b88d9ca43a4423
Johnathan Middleton <jpmawesome@gmail.com>
2020-07-27 02:03:42 -0400

    get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -7,10 +7,23 @@ def primary():
   quotes = f.readlines()
   f.close()
 
+
   last = 13
   rnd = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd].strip('\n'))
 
+  new = int(input('Enter number between 1 and 13: \n'))
+  print(quotes[new].strip('\n'))
+  
+  new2 = input('Enter new string: \n')
+  f = open("quotes.txt", "a")
+  f.write(new2)
+  f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  print(quotes[-1])
+  
 if __name__== "__main__":
   primary()
